0f0f88e844b60b253fb68f99fdf919d95e30b3bb
Stalder <serg_lavinov@inbox.ru>
2020-04-07 21:54:11 +0300

    Profile layout

--- profile.php
+++ profile.php
@@ -19,6 +19,26 @@ include_once "components.php"
     <div class="container">
         <?php renderTopMenu() ?>
         <h3>Профиль</h3>
+        <span>Привет, username! У тебя 4 файла:</span>
+        <div class="row">
+            <?php renderFile("Файл1", "vdsvd") ?>
+            <?php renderFile("Файл2", "vdsvd") ?>
+            <?php renderFile("Файл3", "vdsvd") ?>
+            <?php renderFile("Файл4", "vdsvd") ?>
+        </div>
+
+        <div class="row">
+            <form class="col s8" action="change_password.php" method="POST">
+                <h4>Сменить пароль</h4>
+
+                <input placeholder="Текущий пароль" name="old_password">
+                <input placeholder="Новый пароль" name="new_password">
+                <input placeholder="Новый пароль еще раз" name="new_password_again">
+                <br>
+
+                <input type="submit">
+            </form>
+        </div>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
