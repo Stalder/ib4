483db9a06ac052e84c1e24d9dbcff02a7b7bf897
Stalder <serg_lavinov@inbox.ru>
2020-04-09 16:02:42 +0300

    Brute force protection

--- sign_in.php
+++ sign_in.php
@@ -1,35 +1,41 @@
 <?php
 include_once "utils.php";
 
+redirectProfileIfAuthorized();
+
 $error = false;
 
 $link = connect_mysql();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST["username"] && $_POST["password"]) {
-        $escapedUsername = mysqli_real_escape_string($link, $_POST['username']);
-        $query = mysqli_query($link, "SELECT user_id, user_password FROM users WHERE user_login='" . $escapedUsername . "' LIMIT 1");
-        $data = mysqli_fetch_assoc($query);
+    if ($_POST["username"] && $_POST["password"] && $_POST['captcha']) {
+        session_start();
+        if ($_POST['captcha'] == $_SESSION['digit']) {
+            $escapedUsername = mysqli_real_escape_string($link, $_POST['username']);
+            $query = mysqli_query($link, "SELECT user_id, user_password FROM users WHERE user_login='" . $escapedUsername . "' LIMIT 1");
+            $data = mysqli_fetch_assoc($query);
 
-        if ($data['user_password'] === md5(md5($_POST['password']))) {
-            $hash = md5(generateCode(10));
+            if ($data['user_password'] === md5(md5($_POST['password']))) {
+                $hash = md5(generateCode(10));
 
-            mysqli_query($link, "UPDATE users SET user_hash='" . $hash . "' WHERE user_id='" . $data['user_id'] . "'");
+                mysqli_query($link, "UPDATE users SET user_hash='" . $hash . "' WHERE user_id='" . $data['user_id'] . "'");
 
-            setcookie("id", $data['user_id'], time() + 60 * 60 * 24 * 30, "/");
-            setcookie("hash", $hash, time() + 60 * 60 * 24 * 30, "/", null, null, true); // httponly !!!
+                setcookie("id", $data['user_id'], time() + 60 * 60 * 24 * 30, "/");
+                setcookie("hash", $hash, time() + 60 * 60 * 24 * 30, "/", null, null, true); // httponly !!!
 
-            header("Location: profile.php");
-            exit();
+                header("Location: profile.php");
+                exit();
+            } else {
+                $error = "Вы ввели неправильное имя пользователя или пароль";
+            }
         } else {
-            $error = "Вы ввели неправильное имя пользователя или пароль";
+            $error = "Неверное значение в поле CAPTCHA";
         }
+        session_destroy();
     } else {
         $error = "Введите имя пользователя и пароль";
     }
 }
-
-redirectProfileIfAuthorized();
 
 ?>
 
@@ -59,6 +65,10 @@ redirectProfileIfAuthorized();
 
                 <input placeholder="Имя пользователя" autofocus name="username">
                 <input placeholder="Пароль" type="password" name="password">
+                <br>
+                <br>
+                <img src="/captcha.php" width="120" height="30" alt="CAPTCHA">
+                <input placeholder="CAPTCHA" type="text" size="6" maxlength="5" name="captcha" value="">
 
                 <input type="submit" name="submit">
             </form>
--- sign_up.php
+++ sign_up.php
@@ -7,34 +7,51 @@ redirectProfileIfAuthorized();
 if (isset($_POST['submit'])) {
     $err = [];
 
-    if (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['username'])) {
-        $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+    session_start();
+    if ($_POST['captcha'] == $_SESSION['digit']) {
+        if (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['username'])) {
+            $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+        }
+
+        $rawPassword = $_POST['password'];
+
+        $hasUppercase = preg_match('@[A-Z]@', $rawPassword);
+        $hasLowercase = preg_match('@[a-z]@', $rawPassword);
+        $hasNumber = preg_match('@[0-9]@', $rawPassword);
+        $isLongerThan8 = strlen($rawPassword) > 7;
+
+        if (!$hasUppercase || !$hasLowercase || !$hasNumber || !$isLongerThan8) {
+            $err[] = "Пароль должен быть минимум 8 символов, содержать строчные и заглавные латинские символы и числа";
+        }
+
+        if (strlen($_POST['username']) < 3 || strlen($_POST['username']) > 30) {
+            $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+        }
+
+        if ($_POST['password'] != $_POST['password_again']) {
+            $err[] = "Пароли не совпадают";
+        }
+
+        $link = connect_mysql();
+
+        $escapedLogin = mysqli_real_escape_string($link, $_POST['username']);
+
+        $query = mysqli_query($link, "SELECT user_id FROM users WHERE user_login='" . $escapedLogin . "'");
+        if (mysqli_num_rows($query) > 0) {
+            $err[] = "Пользователь с таким логином уже существует в базе данных";
+        }
+
+        if (count($err) == 0) {
+            $password = md5(md5(trim($_POST['password'])));
+
+            mysqli_query($link, "INSERT INTO users SET user_login='" . $escapedLogin . "', user_password='" . $password . "'");
+            header("Location: sign_in.php");
+            exit();
+        }
+    } else {
+        $err[] = "Неверное значение в поле CAPTCHA";
     }
-
-    if (strlen($_POST['username']) < 3 || strlen($_POST['username']) > 30) {
-        $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
-    }
-
-    if ($_POST['password'] != $_POST['password_again']) {
-        $err[] = "Пароли не совпадают";
-    }
-
-    $link = connect_mysql();
-
-    $escapedLogin = mysqli_real_escape_string($link, $_POST['username']);
-
-    $query = mysqli_query($link, "SELECT user_id FROM users WHERE user_login='" . $escapedLogin . "'");
-    if (mysqli_num_rows($query) > 0) {
-        $err[] = "Пользователь с таким логином уже существует в базе данных";
-    }
-
-    if (count($err) == 0) {
-        $password = md5(md5(trim($_POST['password'])));
-
-        mysqli_query($link, "INSERT INTO users SET user_login='" . $escapedLogin . "', user_password='" . $password . "'");
-        header("Location: sign_in.php");
-        exit();
-    }
+    session_destroy();
 }
 ?>
 
@@ -68,6 +85,10 @@ if (isset($_POST['submit'])) {
                 <input placeholder="Имя пользователя" autofocus name="username">
                 <input placeholder="Пароль" type="password" name="password">
                 <input placeholder="Пароль еще раз" type="password" name="password_again">
+                <br>
+                <br>
+                <img src="/captcha.php" width="120" height="30" alt="CAPTCHA">
+                <input placeholder="CAPTCHA" type="text" size="6" maxlength="5" name="captcha" value="">
 
                 <input type="submit" name="submit">
             </form>
